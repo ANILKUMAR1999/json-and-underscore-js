a209943e4683cda2bf132bc8d62b6b65580c1c79
ANILKUMAR1999 <31931439+ANILKUMAR1999@users.noreply.github.com>
2017-09-14 08:40:38 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 'use strict';
 ( function() {
-	  const inputsElem = document.querySelector('#inputs');
+		const inputsElem = document.querySelector('#inputs1');
+    const inputsElem2 = document.querySelector('#inputs2');
     const outPutElem1 = document.querySelector('#u-js');
     const outPutElem2 = document.querySelector('#n-js');
 
 
-  function stringify(data) {
-    try {
-        data = JSON.stringify(data);
-    } catch(e){
-         console.log(e)
+    function stringify(data) {
+        try {
+            data = JSON.stringify(data);
+        } catch (e) {
+            console.error(e);
+        }
+
+        return data;
     }
-    return data;
-   }
+   
+   const input = 'Range U Given Automatically Numbers Generated';
+   inputsElem.innerText = stringify(input);
 
-// Input Persons Age
-const numbers = [5, 8, 4, 18, 20];
-inputsElem.innerText = stringify(numbers);
 
-function checkAdult(age) {
-	return age >= 18;
-}
+   // Using UnderScore Range Method
+   const result_U = _.range(10);
+   outPutElem1.innerText = stringify(result_U);
 
-// Using UnderScore JS reject Method
-const result_U = _.reject(numbers, checkAdult);
-outPutElem1.innerText = stringify(result_U);
+   // Using JavaScirpt For Loop
+       const result_N = []
+      for (i=0; i<10; i++) {
+      	result_N.push(i)
+      }
+      outPutElem2.innerText = stringify(result_N);
 
-function checkAdul(age,index) {
-	return ((age >= 18) -1);
-}
-
-// Using JavaScript Filter Method
-const result_N = stringify(numbers.filter(checkAdul));
-outPutElem2.innerText = result_N;
 } ());
